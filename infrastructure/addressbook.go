73bf290ea7a7e8699d9b2f894cee0daa38bee522
zcd <zcd@wooduan2019>
2022-10-26 17:41:14 +0800

    完成 FindContains 开发

--- infrastructure/addressbook.go
+++ infrastructure/addressbook.go
@@ -15,7 +15,6 @@ import (
 // 	Take(ctx context.Context, id any) (entity *aggregate.Contact, found bool)
 // 	Put(ctx context.Context, id any, entity *aggregate.Contact)
 // 	Remove(ctx context.Context, id any) (removed *aggregate.Contact, exists bool)
-// 	//模糊查找
 // 	FindContains(ctx context.Context, contains string) ([]*aggregate.Contact, error)
 // 	FindAllForGroup(ctx context.Context, groupId int64) ([]*aggregate.Contact, error)
 // }
@@ -26,5 +25,23 @@ type ContactRepositoryImpl struct {
 }
 
 func (repo *ContactRepositoryImpl) FindContains(ctx context.Context, contains string) ([]*aggregate.Contact, error) {
-	repo.coll.Find(ctx, bson.M{"name": primitive.Regex{Pattern: "*" + contains + "*", Options: "im"}})
+	cursor, err := repo.coll.Find(ctx, bson.M{"name": primitive.Regex{Pattern: "*" + contains + "*", Options: "im"}})
+	if err != nil {
+		return nil, err
+	}
+	var results []bson.D
+	if err = cursor.All(ctx, &results); err != nil {
+		return nil, err
+	}
+	contacts := make([]*aggregate.Contact, 0)
+	for result := range results {
+		var doc []byte
+		if doc, err = bson.Marshal(result); err != nil {
+			return nil, err
+		}
+		contact := &aggregate.Contact{}
+		bson.Unmarshal(doc, contact)
+		contacts = append(contacts, contact)
+	}
+	return contacts, nil
 }
